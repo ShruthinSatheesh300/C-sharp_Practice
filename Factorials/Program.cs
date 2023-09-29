6a9b8d866afa17947af4cc9f511189389f3b95ab
vaibhavDharmik1 <vaibhavdharmik55.com>
2023-09-29 12:57:45 +0530

    Add project files.

---
+++ Factorials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorials
+{
+    internal class Program
+    {
+        
+        static void Main()
+
+        {
+            Console.WriteLine("Enter the number to find its factorial : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int f1 = 1; 
+
+            for (int i = 1; i <= num; i++)
+            {
+                f1 = f1 * i;
+            }
+            Console.WriteLine("{0}! is {1}", num, f1);
+
+            Console.ReadLine();
+        }
+    }
+}
